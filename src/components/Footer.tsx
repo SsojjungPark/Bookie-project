ed54965818940eaaad127ccd195c895ee8f1e01c
Sojung Park <sojung6458@gmail.com>
2023-09-06 15:14:06 +0900

    Footer 레이아웃

---
+++ src/components/Footer.tsx
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <CopyRight>&copy; 2023. Sojung Park. All rights reserved.</CopyRight>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
+
+const FooterContainer = styled.footer`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #1f1f1f;
+`;
+
+const CopyRight = styled.span`
+  width: 1200px;
+  color: var(--white-color);
+  text-align: center;
+  font-size: 14px;
+`;
