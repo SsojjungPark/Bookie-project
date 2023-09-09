7b0bd858cf2cf2df3fed8e7d5022f35db9bddcbd
Sojung Park <sojung6458@gmail.com>
2023-09-09 15:25:19 +0900

    리뷰 페이지 레이아웃

---
+++ src/components/ReviewLists.tsx
@@ -0,0 +1,142 @@
+import styled from '@emotion/styled';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faStar } from '@fortawesome/free-regular-svg-icons';
+import { faStar as fullStar, faHeart as fullHeart } from '@fortawesome/free-solid-svg-icons';
+
+const ReviewList = () => {
+  return (
+    <ReviewItemsUl>
+      <ReviewItemLi>
+        <BookImage
+          src="https://contents.kyobobook.co.kr/sih/fit-in/300x0/pdt/9788958075226.jpg"
+          alt="책"
+          width="150px"
+        />
+
+        <ReviewItemInfo>
+          <BookInfo>
+            <BookTitle>책 제목</BookTitle>
+            <BookWriter>(저자)</BookWriter>
+          </BookInfo>
+
+          <ReviewInfo>
+            <ReviewTitle>리뷰 제목</ReviewTitle>
+            <NicknameDateWrapper>
+              <Nickname>닉네임</Nickname>
+              <Date>작성일</Date>
+            </NicknameDateWrapper>
+          </ReviewInfo>
+
+          <LikeBookmarkWrapper>
+            <LikeArea>
+              <LikeIcon icon={faHeart} />
+              <LikedIcon icon={fullHeart} />
+              <LikesNumber>0</LikesNumber>
+            </LikeArea>
+
+            <BookmarkIcon icon={faStar} />
+            <BookmarkedIcon icon={fullStar} />
+          </LikeBookmarkWrapper>
+        </ReviewItemInfo>
+      </ReviewItemLi>
+    </ReviewItemsUl>
+  );
+
+  return;
+};
+
+export default ReviewList;
+
+const ReviewItemsUl = styled.ul`
+  width: 100%;
+  border-top: 1px solid var(--signup-input);
+`;
+
+const ReviewItemLi = styled.li`
+  height: 310px;
+  display: flex;
+  align-items: center;
+  padding-left: 20px;
+  border-bottom: 1px solid var(--signup-input);
+`;
+
+const BookImage = styled.img`
+  margin-right: 70px;
+`;
+
+const ReviewItemInfo = styled.div`
+  width: 100%;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const BookInfo = styled.div`
+  display: flex;
+  font-size: 16px;
+`;
+
+const BookTitle = styled.div`
+  margin-right: 5px;
+`;
+
+const BookWriter = styled.div``;
+
+const ReviewInfo = styled.div``;
+
+const ReviewTitle = styled.div`
+  font-weight: 600;
+  font-size: 18px;
+  margin-bottom: 12px;
+
+  &: hover {
+    cursor: pointer;
+  }
+`;
+
+const NicknameDateWrapper = styled.div`
+  display: flex;
+  font-size: 15px;
+  color: #595959;
+`;
+
+const Nickname = styled.div`
+  margin-right: 8px;
+`;
+
+const Date = styled.div``;
+
+const LikeBookmarkWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const LikeArea = styled.div`
+  margin-right: 15px;
+  display: flex;
+  align-items: center;
+`;
+
+const LikeIcon = styled(FontAwesomeIcon)`
+  color: var(--red-color);
+  font-size: 18px;
+`;
+
+const LikedIcon = styled(FontAwesomeIcon)`
+  color: var(--red-color);
+  font-size: 18px;
+`;
+
+const LikesNumber = styled.span`
+  margin-left: 5px;
+  font-size: 15px;
+`;
+
+const BookmarkIcon = styled(FontAwesomeIcon)`
+  font-size: 16px;
+`;
+
+const BookmarkedIcon = styled(FontAwesomeIcon)`
+  font-size: 16px;
+`;
---
+++ src/pages/Novel.tsx
@@ -0,0 +1,49 @@
+import styled from '@emotion/styled';
+import Header from '../components/Header';
+import ReviewList from '../components/ReviewLists';
+import Footer from '../components/Footer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
+
+const Novel = () => {
+  return (
+    <ReviewContainer>
+      <Header />
+
+      <ReviewWrapper>
+        <Title>소설</Title>
+
+        <ReviewList />
+
+        <WriteBtn type="button">
+          <FontAwesomeIcon icon={faPen} />
+          글쓰기
+        </WriteBtn>
+      </ReviewWrapper>
+
+      <Footer />
+    </ReviewContainer>
+  );
+};
+
+export default Novel;
+
+const ReviewContainer = styled.div`
+  background-color: var(--white-color;);
+`;
+
+const ReviewWrapper = styled.div`
+  width: 1200px;
+  margin: 0 auto;
+`;
+
+const Title = styled.h2`
+  font-weight: 700;
+  font-size: 27px;
+  margin: 100px 0 50px 0;
+`;
+
+const WriteBtn = styled.button`
+  float: right;
+  margin-top: 40px;
+`;
