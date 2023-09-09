005b1732a3ff155500795ff7fe085b513121840b
Sojung Park <sojung6458@gmail.com>
2023-09-09 15:26:09 +0900

    수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import './App.css';
-import Header from './components/Header';
+import Novel from './pages/Novel';
 // import Signup from './pages/Signup';
 // import Login from './pages/Login';
+// import Header from './components/Header';
+// import Home from './pages/Home';
 
 function App() {
   return (
     <>
       {/* <Signup /> */}
       {/* <Login /> */}
-      <Header />
+      {/* <Home /> */}
+      <Novel />
     </>
   );
 }
