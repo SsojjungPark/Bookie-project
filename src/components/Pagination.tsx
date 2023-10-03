d2721629f51a5b52b5bfabde8d1e2dd1243f3fd7
Sojung Park <sojung6458@gmail.com>
2023-10-04 03:41:15 +0900

    페이지네이션

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+import { useState } from 'react';
+import Pagination from 'react-js-pagination';
+
+const Paging = (page, count, setPages) => {
+  const [page, setPage] = useState<number>(1);
+
+  const handlePageChange = (page: number) => {
+    setPage(page);
+  };
+
+  return (
+    <>
+      <Pagination
+        activePage={page} // 현재 페이지
+        itemsCountPerPage={5} // 한 페이지랑 보여줄 아이템 갯수
+        totalItemsCount={count} // 총 아이템 갯수
+        pageRangeDisplayed={5} // paginator의 페이지 범위
+        prevPageText={'‹'} // "이전"을 나타낼 텍스트
+        nextPageText={'›'} // "다음"을 나타낼 텍스트
+        onChange={handlePageChange} // 페이지 변경을 핸들링하는 함수
+      />
+    </>
+  );
+};
+
+export default Paging;
